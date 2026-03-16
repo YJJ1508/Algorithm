fabffe5a3f61d37a56cb91ccfe6a76264e8d65f8
YJJ1508 <yeonjoo901@gmail.com>
2026-03-16 19:28:59 +0900

    [level 2] Title: 올바른 괄호, Time: 6.70 ms, Memory: 9.39 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,13 @@
+def solution(s):
+    stack = []
+    for i in s:
+        if i == "(":
+            stack.append(i)
+        else: #')'
+            if not stack: 
+                return False
+            else:
+                stack.pop()
+    
+    if stack: return False
+    else: return True
