19fddd5d57feb8e4405d0d2c12b6d69b16ed4321
YJJ1508 <yeonjoo901@gmail.com>
2026-03-21 20:39:52 +0900

    [level 2] Title: 귤 고르기, Time: 45.10 ms, Memory: 25.5 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,17 +1,13 @@
-def solution(k, tan): #다른 종류 최소화
-    result = 0
+from collections import Counter
+def solution(k, tangerine):
+    cnt_tan = Counter(tangerine) 
+
+    #같은 개수가 많은순으로 정렬
+    cnt_tan = sorted(cnt_tan.items(), key=lambda x:x[1], reverse=True)
+    #개수 많은 순으로 차감
+    idx = 0
+    while k > 0:  
+        k -= cnt_tan[idx][1]
+        idx += 1
     
-    #각 숫자 개수 담아주기
-    dp = [0] * (max(tan) + 1)
-    for i in tan:
-        dp[i] += 1
-    #개수가 많은것부터 담기
-    dp.sort(reverse = True) #오름차순 [5: 2 3: 2 3: 2]
-    #k개 만큼 담아주기 
-    for i in dp:
-        if k <= 0:
-            break
-        k -= i
-        result += 1 #종류 카운트
-    
-    return result
+    return idx
