a75fad15353fda073f05c6534a9fee53af7461a6
YJJ1508 <yeonjoo901@gmail.com>
2026-03-24 17:17:17 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,14 +1,17 @@
-#[그리디 방식 문제] 
-# 틀린이유: dp로 풀면 시간초과 n의 크기가 커서 안됨
-
-def solution(n):  #5 -> 4(+1) 2 1 -> 0(+1)
-    answer = 0
-    while n > 0:
-        #순간이동 가능한 경우
+#처음과 마찬가지로 또 dp로 접근해서 틀림(시간초과는 예상하고 루프를 반만큼 돌리는식으로 품..)
+#버트-> 간단히 수학적으로 접근하면 끝(그리디)
+def solution(n):
+    amount = 0
+    while n != 0:
+        #순간이동
         if n % 2 == 0:
-            n /= 2
+            n //= 2
         else:
             n -= 1
-            answer += 1 #이동 수 +1
-            
-    return answer
+            amount += 1
+    
+    return amount
+    
+    
+
+
