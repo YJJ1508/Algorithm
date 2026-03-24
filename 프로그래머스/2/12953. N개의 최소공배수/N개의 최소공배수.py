e2e261a4744c6d900a68680cae991b3750ba1677
YJJ1508 <yeonjoo901@gmail.com>
2026-03-24 19:19:54 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,8 +1,8 @@
-from math import gcd
-def solution(arr):     
-    answer = arr[0] 
-    
+import math
+def solution(arr):         
+    answer = arr[0]
+    #두개씩 lcm 뽑아서 반복 (2,6) -> 6 (6,8)-> 24 (24,14) -> 168
     for num in arr[1:]: 
-        answer = answer*num // gcd(answer, num)
-    
+        answer = answer*num // math.gcd(answer, num)
+
     return answer
