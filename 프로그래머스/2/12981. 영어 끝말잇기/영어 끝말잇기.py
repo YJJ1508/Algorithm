f88d790ce7cc26643cb04252ae5ceb622dd1e230
YJJ1508 <yeonjoo901@gmail.com>
2026-03-25 18:26:43 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,19 +1,15 @@
-def solution(n, words):  #먼저 탈락하는 사람 번호, 몇번째 차례에 탈락
-    answer = [0,0]
-
-    temp = [words[0][0]] 
-    round = 0
-    for i in range(n-1,len(words), n): #각 라운드마다 끊기
-        round += 1
-        for j in range(i+1-n,i+1): # 각 라운드의 단어 순회
-            #통과
-            if words[j] not in temp and temp[-1][-1] == words[j][0] :
-                temp.append(words[j])
-            #탈락하는 경우
-            else:
-                num = (j+1) % n #1//2     
-                answer[0] = n if num == 0 else num #탈락 번호
-                answer[1] = round #탈락 라운드 
-                return answer
-                
-    return answer
+def solution(n, words):
+    check = [words[0]]
+    #words 순회하며 틀린 단어 찾기
+    for i in range(1,len(words)):
+        #나온 단어인지 확인하기
+        if words[i] not in check:
+            check.append(words[i])
+        else:
+            return [i%n+1, i//n+1]
+        
+        #끝말잇기 틀린경우
+        if words[i][0] != words[i-1][-1]:
+            return [i%n+1, i//n+1]
+    
+    return [0,0]
