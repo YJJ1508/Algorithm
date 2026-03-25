7f40d35c18fcf32d47078695deaaf629758e751f
YJJ1508 <yeonjoo901@gmail.com>
2026-03-25 17:22:39 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 3957.39 ms, Memory: 42.6 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,12 +1,14 @@
-def solution(elements): #[1,1,4,7,9]1           
+def solution(elements):
+    nums = set()
+    elem_2 = elements + elements 
     
-    l = len(elements) #원래 길이
-    elements = elements + elements #원형이라 더하기 위함
+    #완전탐색 
+    for i in range(1,len(elements)+1): #길이 
+        for j in range(len(elements)): #길이 i인 연속 부분 수열
+            new = sum(elem_2[j:j+i])
+            nums.add(new)
     
-    num = set(elements) 
-    for i in range(2,l+1): #길이(더할 숫자 개수) 2~5
-        for j in range(0, l):  
-            new = sum(elements[j:j+i])
-            num.add(new)
-            
-    return len(num)
+    return len(nums)
+    
+
+        
