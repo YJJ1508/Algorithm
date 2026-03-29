893f36373d7cf0e8bfc98c2917183bb09dac8268
YJJ1508 <yeonjoo901@gmail.com>
2026-03-29 15:16:45 +0900

    [level 2] Title: 괄호 회전하기, Time: 122.81 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,18 +1,32 @@
-def bracket(s):
-        stack = []
-        for i in s:
-            if len(stack) == 0: stack.append(i)
-            else:
-                if i == ")" and stack[-1] == "(":   stack.pop()
-                elif i == "]" and stack[-1] == "[":   stack.pop()
-                elif i == "}" and stack[-1] == "{":   stack.pop()
-                else: stack.append(i)
-        return 1 if len(stack) == 0 else 0
-        
-def solution(s):
-    answer = 0
+def solution(s):   
+    count = 0
     
+    temp = ''
+    result = []
     for i in range(len(s)):
-        if bracket(s):  answer +=1
-        s = s[1:] + s[:1]
-    return answer
+        #1. s를 x칸만큼 회전
+        temp = s[i:len(s)+1]+s[0:i]  
+        #2. 올바른 괄호 문자열인지 확인
+        stack = []
+        check = True   
+        for j in temp:
+            if not stack:
+                if j == '}' or j ==']' or j==')':
+                    check = False
+                    break
+                stack.append(j)
+            else:
+                if j == '{' or j=='(' or j=="[":
+                    stack.append(j)
+                else:
+                    if stack[-1]=="{" and j =="}":
+                        stack.pop()
+                    elif stack[-1]=="[" and j==']':
+                        stack.pop()
+                    elif stack[-1]=='(' and j==')':
+                        stack.pop()
+        #올바른 괄호 문자열 +1
+        if not stack and check == True:
+            count += 1 
+        
+    return count
