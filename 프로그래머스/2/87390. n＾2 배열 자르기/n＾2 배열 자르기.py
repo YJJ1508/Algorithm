ca692c4b2af218a47813bbd342945e50926f66a7
YJJ1508 <yeonjoo901@gmail.com>
2026-03-29 18:10:17 +0900

    [level 2] Title: n^2 배열 자르기, Time: 48.29 ms, Memory: 18 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -1,9 +1,9 @@
-#틀린이유: 브루트포스 x (시간초과) => 규칙으로 푸는 문제
-
-def solution(n, left, right):    #1 2 [3 2 2 3] 3 3 3  
-    answer = []
-    for i in range(left,right+1):
-        a = i // n
-        b = i % n
-        answer.append(max(a,b)+1)
-    return answer
+#틀린이유: 완전탐색시 시간초과, 규칙으로 풀어야함
+def solution(n, left, right):
+    arr = []
+    for i in range(left, right+1):
+        a = i//n
+        b = i%n
+        arr.append(max(a,b)+1)
+         
+    return arr
